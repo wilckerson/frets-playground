f19223cbe7abac1b19e8a45bfd67f20fb112f9cd
wilckerson.ganda <wilckerson.ganda@judi.ai>
2022-10-04 15:03:13 -0700

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // const initialRatioArray = [9/8,4/3,3/2,16/9];
+  const initialRatioArray = [1, 1, 1, 10 / 9, 5 / 4, 10 / 7, 5 / 3];
+
+  const fretBoardLength = 650;
+  const [ratiosArray, setRatiosArray] = useState(initialRatioArray);
+
+  function onChangeRatio(value, index) {
+    const newArray = [...ratiosArray];
+    newArray[index] = value;
+    setRatiosArray(newArray);
+  }
+
+  // function getRatioValueFromPosition(position) {
+  //   return fretBoardLength / (fretBoardLength - position);
+  // }
+
+  function getPositionFromRatioValue(ratioValue) {
+    return fretBoardLength - fretBoardLength / ratioValue;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="fretboard" style={{ width: fretBoardLength + "px" }}>
+        <div className="fret"></div>
+        {ratiosArray.map((ratio, index) =>
+          ratio === 1 ? null : (
+            <div
+              key={"fret" + index}
+              className="fret"
+              style={{ left: getPositionFromRatioValue(ratio) + "px" }}
+            ></div>
+          )
+        )}
+      </div>
+
+      {ratiosArray.map((ratio, index) => (
+        <div>
+          <span>Fret {index + 1}: </span>
+          <input
+            type="number"
+            min="1"
+            step="0.005"
+            value={ratio}
+            onChange={(ev) => onChangeRatio(ev.target.value, index)}
+          />
+        </div>
+      ))}
     </div>
   );
 }
